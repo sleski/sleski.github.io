d59aca933aa54fb1b8556925f7e6bdea3f298b87
sleski <slawomir.leski@gmail.com>
2026-04-29 13:52:52 +0200

    Added

--- WebComponents/YoutubePreview.js
+++ WebComponents/YoutubePreview.js
@@ -6,7 +6,7 @@ export default class YoutubePreview extends HTMLElement {
   }
 
   connectedCallback() {
-    this.innerHTML = `<div align="${this.alignParam}"><a href="https://www.youtube.com/watch?v=${this.clipID}"><img src="https://img.youtube.com/vi/${this.clipID}/0.jpg" alt="IMAGE ALT TEXT"></a></div><hr/>`;
+    this.innerHTML = `<div align="${this.alignParam}"><a href="https://www.youtube.com/watch?v=${this.clipID}"><img src="https://img.youtube.com/vi/${this.clipID}/0.jpg"></a><div><p>Northern Lights</p></div></div><hr/>`;
   }
 }
 
