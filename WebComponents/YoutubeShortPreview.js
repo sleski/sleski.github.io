5ba34cfb0c7fd7c8e2b0dba8af0ee760a8d160dc
sleski <slawomir.leski@gmail.com>
2026-04-29 14:13:55 +0200

    Added description

--- WebComponents/YoutubeShortPreview.js
+++ WebComponents/YoutubeShortPreview.js
@@ -2,11 +2,12 @@ export default class YoutubeShortPreview extends HTMLElement {
   constructor() {
     super();
     this.clipID = this.getAttribute("clipID");
-    this.alignParam = this.getAttribute("alignParam") || "left";
+    this.alignParam = this.getAttribute("alignParam") || "center";
+    this.textContent = this.getAttribute("description") || ""
   }
 
   connectedCallback() {
-    this.innerHTML = `<div align="${this.alignParam}"><a href="https://www.youtube.com/shorts/${this.clipID}"><img src="https://img.youtube.com/vi/${this.clipID}/0.jpg" alt="IMAGE ALT TEXT"></a></div><hr/>`;
+    this.innerHTML = `<div align="${this.alignParam}"><a href="https://www.youtube.com/shorts/${this.clipID}"><img src="https://img.youtube.com/vi/${this.clipID}/0.jpg"></a><div><p>${this.textContent}</p></div></div><hr/>`;
   }
 }
 
