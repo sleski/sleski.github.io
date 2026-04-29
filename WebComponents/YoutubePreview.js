def009c90b14916317060c54f0b812c5a506a0ca
sleski <slawomir.leski@gmail.com>
2026-04-29 14:03:20 +0200

    Added text below the image

--- WebComponents/YoutubePreview.js
+++ WebComponents/YoutubePreview.js
@@ -7,7 +7,7 @@ export default class YoutubePreview extends HTMLElement {
   }
 
   connectedCallback() {
-    this.innerHTML = `<div align="${this.alignParam}"><a href="https://www.youtube.com/watch?v=${this.clipID}"><img src="https://img.youtube.com/vi/${this.clipID}/0.jpg"></a><div><p>&{this.textContent}</p></div></div><hr/>`;
+    this.innerHTML = `<div align="${this.alignParam}"><a href="https://www.youtube.com/watch?v=${this.clipID}"><img src="https://img.youtube.com/vi/${this.clipID}/0.jpg"></a><div><p>${this.textContent}</p></div></div><hr/>`;
   }
 }
 
